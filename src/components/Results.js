50af17ec1102b7cc8dea5cdc9d33644553ebb4e6
Matthieu Roux <36091631+M4TTRX@users.noreply.github.com>
2021-12-05 21:52:03 +0100

    IMproved UI and query

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,49 +1,58 @@
-import React, {useEffect, useState} from "react";
-import {CardActionArea, Grid, Card, CardContent, CardMedia, Typography} from '@mui/material';
-import {researchQuery} from "../services/sparqlRequests";
+import React, { useEffect, useState } from "react";
+import {
+  CardActionArea,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { researchQuery } from "../services/sparqlRequests";
 import BookResult from "./BookResult";
-import {useNavigate} from "react-router-dom";
-import {useParams} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
+export default function Results({ books }) {
+  let { searchInput } = useParams();
+  let navigate = useNavigate();
+  const [books, setBooks] = useState(null);
 
-export default function Results() {
+  useEffect(() => {
+    const loadResults = async () => {
+      const response = await researchQuery(searchInput, "");
+      console.log(response);
+      setBooks(response);
+    };
+    loadResults();
+    console.log(books);
+  }, []);
 
-    let {searchInput} = useParams();
-    let navigate = useNavigate();
-    const [books, setBooks] = useState(null);
-
-    useEffect(() => {
-        const loadResults = async () => {
-            const response = await researchQuery(searchInput, "");
-            console.log(response);
-            setBooks(response);
-        };
-        loadResults();
-        console.log(books);
-    }, []);
-
-    return (
-        <div>
-            {books===null ? <div>Loading</div> : books.length===0 ? <div>No results</div> :
-                <Grid
-                container
-                spacing={2}
-                direction="row"
-                justifyContent="flex-start"
-                alignItems="flex-start"
-            >
-                {books.map((obj, index) => {
-                    const data = {
-                        title: obj.name.value,
-                        author: obj.authorName.value,
-                        img: obj.imageUrl.value,
-                        releaseDate: obj.releaseDate.value,
-                        bookURI: obj.book.value.split("http://dbpedia.org/resource/")[1],
-                    };
-                    return BookResult(index, data, navigate);
-                })}
-            </Grid>
-            }
-        </div>
-    );
+  return (
+    <div>
+      {books === null ? (
+        <div>Loading</div>
+      ) : books.length === 0 ? (
+        <div>No results</div>
+      ) : (
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+        >
+          {books.map((obj, index) => {
+            const data = {
+              title: obj.name.value,
+              author: obj.authorName.value,
+              img: obj.imageUrl.value,
+              releaseDate: obj.releaseDate.value,
+              bookURI: obj.book.value.split("http://dbpedia.org/resource/")[1],
+            };
+            return BookResult(index, data, navigate);
+          })}
+        </Grid>
+      )}
+    </div>
+  );
 }
