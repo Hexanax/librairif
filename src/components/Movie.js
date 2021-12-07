b75392411dd05b00638417450c0ed2365e3ca828
Allan Guigal <allan.guigal0@gmail.com>
2021-12-07 11:29:48 +0100

    Add the book associated movies

---
+++ src/components/Movie.js
@@ -0,0 +1,25 @@
+
+const Movie = (props) => {
+    return <div className={"movie"}>
+        <span>Movie's title : </span>
+        <span>{props.movie.movie.value}</span>
+        <br/>
+        <span>Resource URI : </span>
+        <span>{props.movie.uri.value}</span>
+        <br/>
+        <span>Producers : </span>
+        <span>{props.movie.producers.value}</span>
+        <br/>
+        <span>Runtime : </span>
+        <span>{timeConvert(props.movie.runtime.value)}</span>
+        <br/>
+    </div>
+}
+
+function timeConvert(num) {
+    let hours = Math.floor(num / 60);
+    let minutes = num % 60;
+    return hours + "h" + minutes;
+}
+
+export default Movie
